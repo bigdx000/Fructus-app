af749510f6b46bbff1b4132254ad0d2bf53a249b
bigdx0009 <bigdx009@gmail.com>
2020-08-11 17:17:45 -0400

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Derek Hudson on 8/11/20.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
