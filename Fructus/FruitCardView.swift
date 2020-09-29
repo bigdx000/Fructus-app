91ae9e72177e48122412ab02c1634e52f10aee94
bigdx000 <bigdx009@gmail.com>
2020-09-29 18:11:35 -0400

    onboarding view

--- Fructus/FruitCardView.swift
+++ Fructus/FruitCardView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct FruitCardView: View {
     // Mark: - Properties
     
+    @State private var isAnimating: Bool = false
+    
     // Mark: - Body
     var body: some View {
         ZStack {
@@ -19,6 +21,7 @@ struct FruitCardView: View {
                     .resizable()
                     .scaledToFit()
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 1.75), radius: 8, x: 6, y: 8)
+                    .scaleEffect(isAnimating ? 1.0 : 0.6)
                 //Fruit: Title
                 Text("Blueberry")
                     .foregroundColor(Color.white)
@@ -37,10 +40,17 @@ struct FruitCardView: View {
                 //Button: Start
                 StartButtonView()
             }//: VSTACK
-            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
-            .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
-            .cornerRadius(20)
+            
         }//: ZSTACK
+        .onAppear {
+            withAnimation(.easeOut(duration: 0.5)) {
+                isAnimating = true
+            }
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
+        .padding(.horizontal, 20)
     }
 }
 
@@ -49,6 +59,7 @@ struct FruitCardView: View {
 struct FruitCardView_Previews: PreviewProvider {
     static var previews: some View {
         FruitCardView()
+            .previewDevice("iPhone 11")
             .previewLayout(.fixed(width: 460, height: 920))
             
             
