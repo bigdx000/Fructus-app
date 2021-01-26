d4d5e50ef2ceab667d00a3a4e78b2f9828b68baf
bigdx000 <bigdx009@gmail.com>
2021-01-25 21:28:06 -0500

    settings part 2

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -23,8 +23,22 @@ struct SettingsView: View {
                 
                 // Section - 1
                 
-                GroupBox(label: Text("Fructus".uppercased())) {
-                    Text("Hello World")
+                GroupBox(
+                    label:
+                        SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
+                ) {
+                    Divider().padding(.vertical, 4)
+                    
+                    HStack(alignment: .center, spacing: 10){
+                        Image("logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80, height: 80)
+                            .cornerRadius(9)
+                    
+                        Text("Most fruits are naturally low in fat, sodium and calories. None have chlesterol. Fruits are sources of many essential nutrients, including potassium, dietary fiber, vitamins, and much more.")
+                            .font(.footnote)
+                    }
                 }
                 
                 // Section - 2
